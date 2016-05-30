c4cfcda3f5a83165602df668aa8b3e96c47029ef
Thomas W <netzberufler@googlemail.com>
2016-05-30 15:24:17 +0200

    moved Magazine Posts Boxed to Pro Version

--- functions.php
+++ functions.php
@@ -228,6 +228,5 @@ require get_template_directory() . '/inc/addons.php';
 require get_template_directory() . '/inc/slider.php';
 
 // include Widget Files
-require get_template_directory() . '/inc/widgets/widget-magazine-posts-boxed.php';
 require get_template_directory() . '/inc/widgets/widget-magazine-posts-columns.php';
 require get_template_directory() . '/inc/widgets/widget-magazine-posts-grid.php';
