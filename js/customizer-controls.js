def6596c98942b7a4dd59849c4ffe272a8a20ece
Thomas W <netzberufler@googlemail.com>
2016-06-13 15:21:05 +0200

    improved code formatting of Javascript files

--- js/customizer-controls.js
+++ js/customizer-controls.js
@@ -8,52 +8,51 @@
 
 ( function( $ ) {
 
-	// Add Theme Links
+	// Add Theme Links.
 	if ('undefined' !== typeof maxwell_theme_links) {
-		
-		// Theme Links Wrapper
-		box = $('<div class="maxwell-theme-links"></div>')
-			.css({
-				'margin-top' : '14px',
-				'padding' : '2px 14px 14px',
-				'line-height': '2',
-				'font-size' : '14px',
-				'clear' : 'both'
-			});
-		
-		title = $('<h3></h3>').text(maxwell_theme_links.title).css({ 'margin-bottom' : '4px' });
-		
-		// Theme Links
-		themePage = $('<a class="maxwell-theme-page"></a>')
-			.attr('href', maxwell_theme_links.themeURL)
-			.attr('target', '_blank')
-			.text(maxwell_theme_links.themeLabel);
-		
-		themeDocu = $('<a class="maxwell-theme-docu"></a>')
-			.attr('href', maxwell_theme_links.docuURL)
-			.attr('target', '_blank')
-			.text(maxwell_theme_links.docuLabel);
-		
-		rateTheme = $('<a class="maxwell-rate-theme"></a>')
-			.attr('href', maxwell_theme_links.rateURL)
-			.attr('target', '_blank')
-			.text(maxwell_theme_links.rateLabel);
-		
-		// Add Links to Box
+
+		// Theme Links Wrapper.
+		box = $( '<div class="maxwell-theme-links"></div>' ).css({
+			'margin-top' : '14px',
+			'padding' : '2px 14px 14px',
+			'line-height': '2',
+			'font-size' : '14px',
+			'clear' : 'both'
+		});
+
+		title = $( '<h3></h3>' ).text( maxwell_theme_links.title ).css( { 'margin-bottom' : '4px' } );
+
+		// Theme Links.
+		themePage = $( '<a class="maxwell-theme-page"></a>' )
+		.attr( 'href', maxwell_theme_links.themeURL )
+		.attr( 'target', '_blank' )
+		.text( maxwell_theme_links.themeLabel );
+
+		themeDocu = $( '<a class="maxwell-theme-docu"></a>' )
+		.attr( 'href', maxwell_theme_links.docuURL )
+		.attr( 'target', '_blank' )
+		.text( maxwell_theme_links.docuLabel );
+
+		rateTheme = $( '<a class="maxwell-rate-theme"></a>' )
+		.attr( 'href', maxwell_theme_links.rateURL )
+		.attr( 'target', '_blank' )
+		.text( maxwell_theme_links.rateLabel );
+
+		// Add Links to Box.
 		content = box
-			.append(title)
-			.append(themePage).append("<br />")
-			.append(themeDocu).append("<br />")
-			.append(rateTheme);
-		
+		.append( title )
+		.append( themePage ).append( "<br />" )
+		.append( themeDocu ).append( "<br />" )
+		.append( rateTheme );
+
 		setTimeout(function () {
-			$('#accordion-panel-maxwell_options_panel .control-panel-content').append(content);
+			$( '#accordion-panel-maxwell_options_panel .control-panel-content' ).append( content );
 		}, 2000);
 
-		// Remove accordion click event
-		$('.maxwell-theme-links a').on('click', function(e) {
+		// Remove accordion click event.
+		$( '.maxwell-theme-links a' ).on('click', function(e) {
 			e.stopPropagation();
 		});
 	}
-	
+
 } )( jQuery );
--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,15 @@
 /**
- * jQuery Slider JS
+ * Flexslider Setup
  *
  * Adds the Flexslider Plugin for the Featured Post Slideshow
  *
  * @package Maxwell
  */
 
-jQuery(document).ready(function($) {
+jQuery( document ).ready(function($) {
 
-	/* Add flexslider to #post-slider div */ 
-	$("#post-slider").flexslider({
+	/* Add flexslider to #post-slider div */
+	$( "#post-slider" ).flexslider({
 		animation: maxwell_slider_params.animation,
 		slideshowSpeed: maxwell_slider_params.speed,
 		namespace: "zeeflex-",
@@ -18,5 +18,5 @@ jQuery(document).ready(function($) {
 		pauseOnHover: true,
 		controlsContainer: ".post-slider-controls"
 	});
-	
+
 });
