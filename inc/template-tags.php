8c48c32cc51952c27cf73171f9264d36867e2934
Thomas W <netzberufler@googlemail.com>
2016-06-19 17:13:45 +0200

    improved accessibility of post navigation and pagination

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -275,7 +275,10 @@ function maxwell_post_navigation() {
 
 	if ( true === $theme_options['post_navigation'] ) {
 
-		the_post_navigation( array( 'prev_text' => '&laquo; %title', 'next_text' => '%title &raquo;' ) );
+		the_post_navigation( array(
+			'prev_text' => '<span class="screen-reader-text">' . esc_html_x( 'Previous Post:', 'post navigation', 'maxwell' ) . '</span>%title',
+			'next_text' => '<span class="screen-reader-text">' . esc_html_x( 'Next Post:', 'post navigation', 'maxwell' ) . '</span>%title',
+		) );
 
 	}
 
@@ -335,8 +338,8 @@ function maxwell_pagination() {
 		'format' => '?paged=%#%',
 		'current' => max( 1, get_query_var( 'paged' ) ),
 		'total' => $wp_query->max_num_pages,
-		'next_text' => '&raquo;',
-		'prev_text' => '&laquo',
+		'next_text' => '<span class="screen-reader-text">' . esc_html_x( 'Next Posts', 'pagination', 'maxwell' ) . '</span>&raquo;',
+		'prev_text' => '&laquo<span class="screen-reader-text">' . esc_html_x( 'Previous Posts', 'pagination', 'maxwell' ) . '</span>',
 		'add_args' => false,
 	) );
 
