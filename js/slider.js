6227b377df3eb58732510a947bffebcbd477700d
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-04-24 12:01:48 +0200

    improved CSS files and RTL styles

--- js/slider.js
+++ js/slider.js
@@ -14,7 +14,7 @@ jQuery( document ).ready(function($) {
 		slideshowSpeed: maxwell_slider_params.speed,
 		namespace: "zeeflex-",
 		selector: ".zeeslides > li",
-		smoothHeight: true,
+		smoothHeight: false,
 		pauseOnHover: true,
 		controlsContainer: ".post-slider-controls"
 	});
