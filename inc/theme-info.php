5b95f418f2fe7c21e66ac38660a6b52a7d887f64
Thomas W <kontakt@themecoder.de>
2018-04-13 19:07:17 +0200

    moved assets into new folder

--- inc/theme-info.php
+++ inc/theme-info.php
@@ -172,7 +172,7 @@ function maxwell_theme_info_page_css( $hook ) {
 	}
 
 	// Embed theme info css style.
-	wp_enqueue_style( 'maxwell-theme-info-css', get_template_directory_uri() . '/css/theme-info.css' );
+	wp_enqueue_style( 'maxwell-theme-info-css', get_template_directory_uri() . '/assets/css/theme-info.css' );
 
 }
 add_action( 'admin_enqueue_scripts', 'maxwell_theme_info_page_css' );
