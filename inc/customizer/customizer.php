5fca3cc08d3709bf4e2ff175e7f931c562b5f1f8
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-04-21 11:39:11 +0200

    renamed customize-preview.js file

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -52,7 +52,7 @@ add_action( 'customize_register', 'maxwell_customize_register_options' );
  * Embed JS file to make Theme Customizer preview reload changes asynchronously.
  */
 function maxwell_customize_preview_js() {
-	wp_enqueue_script( 'maxwell-customizer-preview', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), '20200410', true );
+	wp_enqueue_script( 'maxwell-customizer-preview', get_template_directory_uri() . '/assets/js/customize-preview.js', array( 'customize-preview' ), '20200410', true );
 }
 add_action( 'customize_preview_init', 'maxwell_customize_preview_js' );
 
