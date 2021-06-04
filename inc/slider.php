aa2fea61c699707ad40b617e730bb06959900923
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-06-04 11:54:15 +0200

    updated flexslider css

--- inc/slider.php
+++ inc/slider.php
@@ -27,11 +27,7 @@ function maxwell_slider_scripts() {
 		// Register and enqueue slider setup.
 		wp_enqueue_script( 'maxwell-slider', get_template_directory_uri() . '/assets/js/slider.js', array( 'jquery-flexslider' ), '20170421' );
 
-		// Register and enqueue slider CSS.
-		wp_enqueue_style( 'maxwell-slider', get_template_directory_uri() . '/assets/css/flexslider.css', array(), '20210303' );
-
 	endif;
-
 }
 add_action( 'wp_enqueue_scripts', 'maxwell_slider_scripts' );
 
